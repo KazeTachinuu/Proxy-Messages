a1a2e61c8e14ce8f587697a27e3bdf786bd8dfe7
Hugo Sibony <hugo.sibony@epita.fr>
2023-11-28 20:39:35 +0100

    Test case 1 success

--- src/ProxyServer.cpp
+++ src/ProxyServer.cpp
@@ -4,7 +4,8 @@
 #include <iostream>
 
 ProxyServer::ProxyServer()
-    : acceptor_(io_context_, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 12345))
+    : acceptor_(io_context_, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 12345)),
+    waitingTimer_(io_context_)
 {
 }
 
@@ -22,18 +23,39 @@ void ProxyServer::startAccept()
             {
                 std::cout << "Proxy Server: User connected.\n";
 
+                // If there's a waiting user, notify the waiting user about the new connection
+                if (waitingUser_)
+                {
+                    notifyUser(waitingUser_, "\nAnother user has connected.");
+                    waitingTimer_.cancel();
+                }
+
                 // Store the connected user socket
                 auto newUserSocket = std::make_shared<boost::asio::ip::tcp::socket>(std::move(userSocket));
                 userSockets_.push_back(newUserSocket);
 
-                // Notify only the connected user about the existing users
+                // Notify the connected user about the existing users
                 if (userSockets_.size() > 1)
                 {
-                    notifyUser(newUserSocket, "Other users are already connected.");
+                    notifyUser(newUserSocket, "\nOther users are already connected.");
                 }
                 else
                 {
-                    notifyUser(newUserSocket, "No other users are currently connected. Waiting for connections.");
+                    notifyUser(newUserSocket, "\nNo other users are currently connected. Waiting 30 seconds for connections.");
+                    // Start waiting timer for the first user
+                    waitingUser_ = newUserSocket;
+                    waitingTimer_.expires_from_now(boost::posix_time::seconds(30));
+                    waitingTimer_.async_wait([this](const boost::system::error_code &error) {
+                        if (!error)
+                        {
+                            std::cout << "No other user connected within 30 seconds. Closing waiting user connection.\n";
+                            // Notify the waiting user about the timeout
+                            notifyUser(waitingUser_, "\nNo other user connected within 30 seconds. Closing connection.");
+                            // Close the socket
+                            waitingUser_->close();
+                            waitingUser_ = nullptr;
+                        }
+                    });
                 }
 
                 // Handle the connected user here if needed
--- src/ProxyServer.hpp
+++ src/ProxyServer.hpp
@@ -17,12 +17,17 @@ public:
 private:
     void startAccept();
     void notifyUser(const std::shared_ptr<boost::asio::ip::tcp::socket>& userSocket, const std::string& message);
+    void handleWaitingUser(const std::shared_ptr<boost::asio::ip::tcp::socket>& waitingUser);
 
     boost::asio::io_context io_context_;
     boost::asio::ip::tcp::acceptor acceptor_;
 
     // Store connected user sockets
     std::vector<std::shared_ptr<boost::asio::ip::tcp::socket>> userSockets_;
+
+    // Timer for waiting user
+    boost::asio::deadline_timer waitingTimer_;
+    std::shared_ptr<boost::asio::ip::tcp::socket> waitingUser_;
 };
 
 #endif // PROXY_SERVER_HPP
